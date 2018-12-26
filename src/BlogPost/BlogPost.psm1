3d8d347462871feb6ac6ac43bc7e4ff3e21440e8
Jesse Harris <jpharris@usc.edu.au>
2018-12-27 09:20:21 +1000

    Added more functions and tests

--- src/BlogPost/BlogPost.psm1
+++ src/BlogPost/BlogPost.psm1
@@ -1,38 +1,5 @@
 #! /usr/bin/env pwsh
 
-[CmdletBinding()]
-Param(
-    [Parameter(ParameterSetName="Post")]
-        [switch]$Post,
-    [Parameter(ParameterSetName="Edit")]
-        [switch]$Edit,
-    [Parameter(ParameterSetName="Delete")]
-        [switch]$Delete,
-    [Parameter(ParameterSetName="Rebuild")]
-        [switch]$Rebuild,
-    [Parameter(ParameterSetName="Reset")]
-        [switch]$Reset,
-    [Parameter(ParameterSetName="List")]
-        [switch]$List,
-    [Parameter(ParameterSetName="Tags")]
-        [switch]$Tags,
-    [Parameter(ParameterSetName="Post")]
-    [Parameter(ParameterSetName="Edit",Mandatory=$True)]
-    [Parameter(ParameterSetName="Delete")]
-    [ValidateScript({Test-Path -Path $_})]
-        [string]$FileName,
-    [Parameter(ParameterSetName="Post")]
-        [switch]$HTML,
-    [Parameter(ParameterSetName="Edit")]
-    [Parameter(ParameterSetName="Name")]
-    [Parameter(ParameterSetName="Tags")]
-        [switch]$NewName,
-    [Parameter(ParameterSetName="Edit")]
-    [Parameter(ParameterSetName="Full")]
-        [switch]$FullFile
-     )
-
-
 function Get-GlobalVariables {
     $Script:global_software_name='PwshBlog'
     $Script:global_software_version='0.1'
@@ -45,11 +12,11 @@ function Get-GlobalVariables {
     $Script:global_url="http://example.com/blog"
 
     # Your name
-    $Script:global_author="Matt Smith"
+    $Script:global_author="John Smith"
     # You can use twitter or facebook or anything for global_author_url
     $Script:global_author_url="http://twitter.com/example" 
     # Your email
-    $Script:global_email="matt@smith.com"
+    $Script:global_email="john@smith.com"
 
     # CC by-nc-nd is a good starting point, you can change this to "&copy;" for Copyright
     $Script:global_license="CC by-nc-nd"
@@ -87,7 +54,7 @@ function Get-GlobalVariables {
 
     # Non blogpost files. Bashblog will ignore these. Useful for static pages and custom content
     # Add them as a bash array, e.g. non_blogpost_files=("news.html" "test.html")
-    $Script:non_blogpost_files=@()
+    $Script:non_blogpost_files=@('about.html','links.html','gnu-linux.html','macos.html','the-bsds.html','my-setup.html','todo.html','visitors.html')
 
     # feed file (rss in this case)
     $Script:blog_feed="feed.rss"
@@ -120,7 +87,7 @@ function Get-GlobalVariables {
     # leave empty to use generated
     $Script:css_include=@()
     # HTML files to exclude from index, f.ex. post_exclude=('imprint.html 'aboutme.html')
-    $Script:html_exclude=@()
+    $Script:html_exclude=@('windows.html','scripts.html','visitors.html')
 
     # Localization and i18n
     # "Comments?" (used in twitter link after every post)
@@ -153,11 +120,12 @@ function Get-GlobalVariables {
     
     # The locale to use for the dates displayed on screen
     $Script:date_format="%B %d, %Y"
-    $Script:date_locale="C"
+    $Script:date_format="MMMM dd, yyyy"
+    $Script:date_locale=(Get-Culture).Name
     $Script:date_inpost="bashblog_timestamp"
     # Don't change these dates
-    $Script:date_format_full="%a, %d %b %Y %H:%M:%S %z"
-    $Script:date_format_timestamp="%Y%m%d%H%M.%S"
+    $Script:date_format_full="dddd, dd MMM yyyy HH:mm:ss zzzz"
+    $Script:date_format_timestamp="yyyyMMddHHmm.ss"
     $Script:date_allposts_header="%B %Y"
     
     <#
@@ -183,6 +151,7 @@ function Get-HTMLFileContent {
         [switch]$Cut,
         [Parameter(ValueFromPipeLine=$True)][string]$Content
     )
+    #Begin {Get-GlobalVariables}
     Process {
         If ($_ -match "<!-- $Start begin -->"){
             $Capture = $True
@@ -199,9 +168,195 @@ function Get-HTMLFileContent {
     }
 }
 
-function Edit-Post {
+function Test-BoilerplateFile {
+    Param($FilePath)
+
+    #Get-GlobalVariables
+    $Name = (Get-Item $FilePath).Name
+    If ($Name -in $Script:non_blogpost_files) { return $True }
+
+    Switch ($Name) {
+        {$_ -in $index_file,$archive_index,$tags_index,$footer_file,$header_file,$global_analytics_file} { return $True }
+        {$_ -match $prefix_tags} { return $True }
+        Default {
+            If ($Name -in $html_exclude) { return $True } 
+            return $False
+        }
+    }
+}
+
+function New-HTMLPage {
+[CmdLetBinding(DefaultParameterSetName='NoIndex')]
+Param(
+    [ValidateScript({Test-Path -Path $_})]
+    [Parameter(Mandatory=$True)]
+    [string]$Content,
+    [Parameter(Mandatory=$True)]$FileName,
+    [Parameter(Mandatory=$True)]$Title,
+    [Parameter(ParameterSetName='Index')][switch]$Index,
+    [Parameter(ParameterSetName='NoIndex')]$Timestamp,
+    [Parameter(ParameterSetName='NoIndex')]$Author
+)
+
+    Invoke-Command -ScriptBlock {
+        Get-Content '.header.html'
+        Write-Output "<title>$Title</title>"
+        # google_analytics
+        #twitter_card "$content" "$title"
+        Write-Output "</head><body>"
+        # stuff to add before the actual body content
+        If ($Script:body_begin_file) { Get-Content "$Script:body_begin_file" }
+        # body divs
+        Write-Output '<div id="divbodyholder">'
+        Write-Output '<div class="headerholder"><div class="header">'
+        # blog title
+        Write-Output '<div id="title">'
+        Get-Content .title.html
+        Write-Output '</div></div></div>' # title, header, headerholder
+        Write-Output '<div id="divbody"><div class="content">'
+
+        $FileUrl=(Get-Item $FileName).Name
+        $FileUrl=$FileUrl.Replace('\\\.rebuilt','') # get the correct url when rebuilding
+        # one blog entry
+        if (!$Index) {
+            Write-Output '<!-- entry begin -->' # marks the beginning of the whole post
+            Write-Output "<h3><a class=`"ablack`" href=`"$FileUrl`">"
+            # remove possible <p>'s on the title because of markdown conversion
+            $Title -match '<[pP]>(?<title>.*)</[pP]>' | Out-Null
+            If ($Matches) { $Title=$Matches['title'] }
+            Write-Output "$Title"
+            Write-Output '</a></h3>'
+            if (!$Timestamp) {
+                Write-Output "<!-- $date_inpost`: #$(Get-Date -Format "$Script:date_format_timestamp")# -->"
+                #Write-Output "<!-- $date_inpost: #$((Get-Date -Format "$Script:date_format_timestamp") -replace '^([A-Z][a-z]{2})\w+,\s(\d\d\s\w{3}\s\d{4}\s\d\d:\d\d:\d\d)\s(.\d\d):(\d\d)','$1, $2$3$4')# -->"
+            } else {
+                Write-Output "<!-- $date_inpost`: #$(Get-Date $Timestamp -Format "$Script:date_format_timestamp")# -->"
+            }
+            if (!$Timestamp) {
+                $DivOutput = "<div class=`"subtitle`">$(Get-Date -Format "$Script:date_format")"
+            } else {
+                $DivOutput = "<div class=`"subtitle`">$(Get-Date $Timestamp -Format "$Script:date_format")"
+            }
+
+            If ($Author) { $DivOutput += " &mdash; `n$Author`n" }
+            Write-Output "$DivOutput</div>"
+            Write-Output '<!-- text begin -->' # this marks the text body, after the title, date...
+        }
+        Get-Content $Content # actual content
+        if (!$Index) {
+            Write-Output "`n<!-- text end -->"
+
+            #twitter "$global_url/$file_url"
+
+            Write-Output '<!-- entry end -->' # absolute end of the post
+        }
+
+        Write-Output '</div>' # content
+
+        # Add disqus commments except for index and all_posts pages
+        #If (!$Index) { disqus_body }
+
+        # page footer
+        Get-Content .footer.html
+        # close divs
+        Write-Output '</div></div>' # divbody and divbodyholder 
+        #disqus_footer
+        If ($Script:body_end_file) { Get-Content "$body_end_file" }
+        Write-Output '</body></html>'
+    } | Out-File "$FileName"
+}
+
+function New-Includes {
+    Invoke-Command -ScriptBlock {
+        Write-Output "<h1 class=`"nomargin`"><a class=`"ablack`" href=`"$Script:global_url/$Script:index_file`">$Script:global_title</a></h1>" 
+        Write-Output "<div id=`"description`">$global_description</div>"
+    } | Out-File ".title.html"
+
+    if ($Script:header_file -and (Test-Path -Path $Script:header_file)) {
+        Copy-Item "$Script:header_file" .header.html
+    } else {
+        Invoke-Command -ScriptBlock {
+            Write-Output '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">'
+            Write-Output '<html xmlns="http://www.w3.org/1999/xhtml"><head>'
+            Write-Output '<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />'
+            Write-Output '<meta name="viewport" content="width=device-width, initial-scale=1.0" />'
+            $Script:css_include | ForEach-Object { "<link rel=`"stylesheet`" href=`"$_`" type=`"text/css`" />"}
+            if (!$Script:global_feedburner) {
+                Write-Output "<link rel=`"alternate`" type=`"application/rss+xml`" title=`"$Script:template_subscribe_browser_button`" href=`"$Script:blog_feed`" />"
+            } else { 
+                Write-Output "<link rel=`"alternate`" type=`"application/rss+xml`" title=`"$Script:template_subscribe_browser_button`" href=`"$Script:global_feedburner`" />"
+            }
+        } | Out-File ".header.html"
+    }
+
+    if ($Script:footer_file -and (Test-Path -Path $Script:footer_file)) {
+        Copy-Item "$Script:footer_file" .footer.html
+    } else {
+        Invoke-Command -ScriptBlock {
+            $protected_mail=$Script:global_email -replace '@','&#64;'
+            $protected_mail=$protected_mail -replace '\.','&#46;'
+            Write-Output "<div id=`"footer`">$Script:global_license <a href=`"$Script:global_author_url`">$Script:global_author</a> &mdash; <a href=`"mailto:$protected_mail`">$protected_mail</a><br/>"
+            Write-Output 'Generated with <a href="https://github.com/cfenollosa/bashblog">bashblog</a>, a single bash script to easily create blogs like this one</div>'
+        } | Out-File -Append ".footer.html"
+    }
+}
+
+function New-CSS {
+    If ($Script:css_include.Count -gt 0) { return } else {
+        $Script:css_include = @('main.css', 'blog.css')
+    }
+
+    If (!(Test-Path -Path blog.css)) {
+        # blog.css directives will be loaded after main.css and thus will prevail
+        Write-Output '#title{font-size: x-large;}
+        a.ablack{color:black !important;}
+        li{margin-bottom:8px;}
+        ul,ol{margin-left:24px;margin-right:24px;}
+        #all_posts{margin-top:24px;text-align:center;}
+        .subtitle{font-size:small;margin:12px 0px;}
+        .content p{margin-left:24px;margin-right:24px;}
+        h1{margin-bottom:12px !important;}
+        #description{font-size:large;margin-bottom:12px;}
+        h3{margin-top:42px;margin-bottom:8px;}
+        h4{margin-left:24px;margin-right:24px;}
+        img{max-width:100%;}
+        #twitter{line-height:20px;vertical-align:top;text-align:right;font-style:italic;color:#333;margin-top:24px;font-size:14px;}' | Out-File blog.css
+    }
+
+    # If there is a style.css from the parent page (i.e. some landing page)
+    # then use it. This directive is here for compatibility with my own
+    # home page. Feel free to edit it out, though it doesn't hurt
+    If ((Test-Path -Path ../style.css) -and !(Test-Path -Path main.css)) {
+        ln -s "../style.css" "main.css" 
+    } elseif (!(Test-Path -Path main.css)) {
+        Write-Output 'body{font-family:Georgia,"Times New Roman",Times,serif;margin:0;padding:0;background-color:#F3F3F3;}
+        #divbodyholder{padding:5px;background-color:#DDD;width:100%;max-width:874px;margin:24px auto;}
+        #divbody{border:solid 1px #ccc;background-color:#fff;padding:0px 48px 24px 48px;top:0;}
+        .headerholder{background-color:#f9f9f9;border-top:solid 1px #ccc;border-left:solid 1px #ccc;border-right:solid 1px #ccc;}
+        .header{width:100%;max-width:800px;margin:0px auto;padding-top:24px;padding-bottom:8px;}
+        .content{margin-bottom:5%;}
+        .nomargin{margin:0;}
+        .description{margin-top:10px;border-top:solid 1px #666;padding:10px 0;}
+        h3{font-size:20pt;width:100%;font-weight:bold;margin-top:32px;margin-bottom:0;}
+        .clear{clear:both;}
+        #footer{padding-top:10px;border-top:solid 1px #666;color:#333333;text-align:center;font-size:small;font-family:"Courier New","Courier",monospace;}
+        a{text-decoration:none;color:#003366 !important;}
+        a:visited{text-decoration:none;color:#336699 !important;}
+        blockquote{background-color:#f9f9f9;border-left:solid 4px #e9e9e9;margin-left:12px;padding:12px 12px 12px 24px;}
+        blockquote img{margin:12px 0px;}
+        blockquote iframe{margin:12px 0px;}' | Out-File main.css
+    }
+}
+
+function Edit-BlogPost {
 
 }
+
+function New-BlogPost {
+
+}
+
+
 
 function do_main {
     [CmdLetBinding()]
@@ -209,9 +364,10 @@ function do_main {
     Get-GlobalVariables
     $global_config=".config"
     Write-Verbose "Running version $global_software_version"
-
+    New-CSS
 }
 
+Get-GlobalVariables
 #
 # MAIN
 # Do not change anything here. If you want to modify the code, edit do_main
